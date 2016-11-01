449a02d68c170d1909a0afe81a959b85177712f9
Nick Sutterer <apotonick@gmail.com>
2016-11-01 20:18:55 +1100

    no warning from bundler.

--- lib/rspec/trailblazer.rb
+++ lib/rspec/trailblazer.rb
@@ -1,4 +1,4 @@
-require "rspec/trailblazer/version"
+require "rspec"
 
 module RSpec
   module Trailblazer
@@ -8,15 +8,21 @@ module RSpec
         def initialize(result)
           super(result)
 
-          raise "Policy breach: #{result["policy.result"]}" unless result["policy.result"][:valid]
+          raise "Policy breach: #{result["policy.result"]}" unless result["policy.result"]["valid"]
         end
       end
     end
 
     module Rspec
       module Helpers
-        def factory(name, method_name, &block)
-          let(name) { Testing::Result.new( instance_exec(&block) ) }
+        def factory(name, method_name=nil, &block)
+          # TODO: make rspec raise the exception from the method using the factory, not just the line, `because Association::Archive.({ id: model.id })` won't tell you what went wrong.
+          let(name) {
+            result = Testing::Result.new( instance_exec(&block) )
+
+            return result[method_name] if method_name
+            result
+          }
         end
       end
     end
